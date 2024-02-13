e7f6dfd64a017001a80f2dad80a82217bbda18c4
Shahriar Tamim <127070914+shahriartamim2@users.noreply.github.com>
2024-02-14 00:11:41 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let navbar = document.querySelector('nav')
+let menuLinks = document.getElementById("menu-links")
+
+function toggleMenu(){
+    menuLinks.classList.toggle('show-menu')
+}
+
+window.onscroll = function(){
+    if(window.scrollY > 0){
+        navbar.style.background = '#eefff9';
+    }else{
+        navbar.style.background = 'transparent'
+    }
+
+}
